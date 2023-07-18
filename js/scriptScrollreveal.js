47166ea2c9ae84a042c5168120fb5385c8e282df
Nelson <nelson070585@gmail.com>
2023-07-18 00:26:31 -0300

    Primer subida de proyecto, con cosas por editar

---
+++ js/scriptScrollreveal.js
@@ -0,0 +1,51 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scrollmenu', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.scrollBanner', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+    
+    sr.reveal('.scrollSobreMi', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+
+    sr.reveal('.scrollProyectos', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+
+    sr.reveal('.scrollSoftskills', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+
+    sr.reveal('.scrollFormaciones', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+    
+    sr.reveal('.scrollBoton', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+
+
+    sr.reveal('.scrollFormulario', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px'
+    });
+
